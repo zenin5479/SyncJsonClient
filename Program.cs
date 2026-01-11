31f2c0d29688abad434a938e5aa0dd036e399220
zenin5479 <zenin5479@gmail.com>
2026-01-11 10:17:30 +0300

    Update 8.0 Обновлен метод. Проведен рефакторинг. Рабочий код.

--- Program.cs
+++ Program.cs
@@ -308,7 +308,7 @@ namespace SyncJsonClient
                         using (StreamReader reader = new StreamReader(stream))
                         {
                            string error = reader.ReadToEnd();
-                           Console.WriteLine(string.Format("Сообщение об ошибке: {0}", error));
+                           Console.WriteLine("Сообщение об ошибке: {0}", error);
                         }
                   }
                }
