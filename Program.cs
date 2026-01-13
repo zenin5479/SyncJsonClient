6e5792887b5df5471d5e180d3306ae551b9502d5
zenin5479 <zenin5479@gmail.com>
2026-01-13 09:39:32 +0300

    Update 11.2 Обновлен метод. Проведен рефакторинг. Рабочий код.

--- Program.cs
+++ Program.cs
@@ -10,7 +10,6 @@ namespace SyncJsonClient
    public class Item
    {
       public int Id { get; set; }
-
       public string vendor { get; set; }
       public string Name { get; set; }
       public double Price { get; set; }
