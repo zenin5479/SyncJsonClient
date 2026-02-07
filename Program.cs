dc24a7f2d4fb49cd18a6fc2cf668a3562b1cf7a0
zenin5479 <zenin5479@gmail.com>
2026-02-07 22:32:01 +0300

    Update 15.5 Обновлен метод. Проведен рефакторинг. Рабочий код.

--- Program.cs
+++ Program.cs
@@ -86,8 +86,8 @@ namespace SyncJsonClient
          Console.WriteLine("Конвертация из Timestamp в DateTime через DateTimeOffset");
          DateTime dateTime = DateTimeOffset.FromUnixTimeMilliseconds(timestampOne).UtcDateTime;
          Console.WriteLine("Текущее UTC время: {0}", dateTime);
-         Console.WriteLine("Текущее UTC время в милисекундах: {0:dd.MM.yyyy HH:mm:ss.fff}", dateTime); 
-         
+         Console.WriteLine("Текущее UTC время в милисекундах: {0:dd.MM.yyyy HH:mm:ss.fff}", dateTime);
+
          Console.ReadKey();
 
          Console.WriteLine("SyncJsonClient Тест");
