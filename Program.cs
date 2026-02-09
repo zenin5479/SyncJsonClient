4f3d78dc41d738d264852344cfe8d686edd2bfcc
zenin5479 <zenin5479@gmail.com>
2026-02-09 11:48:46 +0300

    Update 18.6 Обновлен метод. Проведен рефакторинг. Рабочий код.

--- Program.cs
+++ Program.cs
@@ -67,7 +67,7 @@ namespace SyncJsonClient
          Console.WriteLine("3. Время (в формате строки): {0}", deserializedeven.Date.ToString("dd.MM.yyyy HH:mm:ss.fff"));
          Console.WriteLine("4. Unix timestamp (ms): {0}", deserializedeven.Timestamp);
 
-         Console.WriteLine("Timestamp: {0}", timestampOne);
+         
          Console.WriteLine();
 
          Console.ReadKey();
